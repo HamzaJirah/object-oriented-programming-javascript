e5c3df5ad3de590b1c674999f6ff385b550f9ea8
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 20:36:17 +0100

    logged getAge to the console

--- object_prototypes.js
+++ object_prototypes.js
@@ -12,10 +12,11 @@ Student.prototype.getSummary = function (){
 
 //getAge
 Student.prototype.getAge = function(){
-  const years = new Date().getFullYear - this.year;
+  const years = new Date().getFullYear() - this.year;
+  return `${this.name} has been the ${this.prefect} for ${years} years`
 }
 
 const student1 = new Student("Abraham Isa'ac", 'headboy', '2000')
 console.log(student1.getSummary());
-console.log(student1);
+console.log(student1.getAge());
 
