5d14aad49153f3033707179a32f9a833002fd46c
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 19:13:08 +0100

    created a function

--- object_prototypes.js
+++ object_prototypes.js
@@ -1,11 +1,5 @@
-//Object Constructor
-
+//Object prototype
 function Student(name, prefect, age){
-  this.name = name;
-  this.prefect = prefect;
-  this.age = age;
-  this.getSummary = function (){
-    return `${this.name} was the ${this.prefect} at age ${this.age}`;
-  }
+  
 }
 
