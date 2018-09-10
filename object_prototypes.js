5e5aea29e3d28d3ccf9c30375074f5736e4131b8
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 20:24:37 +0100

    changed object property

--- object_prototypes.js
+++ object_prototypes.js
@@ -1,16 +1,16 @@
 //Object prototype
-function Student(name, prefect, age){
+function Student(name, prefect, year){
   this.name = name;
   this.prefect = prefect;
-  this.age = age;
+  this.year = year;
 }
 
 //get summary
 Student.prototype.getSummary = function (){
-  return `${this.name} has been the ${this.prefect} since age ${this.age}`
+  return `${this.name} has been the ${this.prefect} since ${this.year}`
 }
 
-const student1 = new Student("Abraham Isa'ac", 'headboy', '19')
+const student1 = new Student("Abraham Isa'ac", 'headboy', '2000')
 console.log(student1.getSummary());
 console.log(student1);
 
