fc7dd3cc7aba643e3ca05b266143c0281e720a7c
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 19:03:42 +0100

    logged to the console

--- object_constructor.js
+++ object_constructor.js
@@ -14,5 +14,5 @@ const student1 = new Student('Phil Jeff', 'time-keeper', '15');
 const student2 = new Student('Kenneth Mike', 'labour-captain', '16');
 
 //log to console
-console.log(student1.name);
-console.log(student2.age);
+console.log(student1.getSummary());
+console.log(student2.getSummary());
