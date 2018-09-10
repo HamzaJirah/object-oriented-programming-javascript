5200f471fb06e5c50bd12f9f9d34905dca823209
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 20:19:27 +0100

    logged to the console

--- object_prototypes.js
+++ object_prototypes.js
@@ -9,7 +9,7 @@ function Student(name, prefect, age){
 Student.prototype.getSummary = function (){
   return `${this.name} has been the ${this.prefect} since age ${this.age}`
 }
-
+c
 const student1 = new Student("Abraham Isa'ac", 'headboy', '19')
 console.log(student1.getSummary());
 
