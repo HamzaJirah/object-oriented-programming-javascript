bdaf7cb5a99a30f010ee02ba407341f18f24c2fe
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 14:19:48 +0100

    created another object

--- object_literals.js
+++ object_literals.js
@@ -1,11 +1,21 @@
 //create book object
+
+//Object-Literal
 const student1 = {
   name: 'Musa Segun',
   title: 'prefect',
   year: '2000',
   getSummary: function (){
-    return '${this.name} was the ${this.title} in ${this.year}';
+    return `${this.name} was the ${this.title} in ${this.year}`;
   }
 }
-console.log(student1);
-console.log(student1.title);
+
+const student2 = {
+  name: 'Chinedu Gyang',
+  title: 'class rep',
+  year: '2015',
+  getSummary: function (){
+    return `${this.name} was the ${this.title} in ${this.year}`;
+  }
+}
+console.log(student1.getSummary());
