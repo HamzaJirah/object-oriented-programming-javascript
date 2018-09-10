089b64d590bd3a3e2d34dc4583c31e80666967a3
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 14:13:48 +0100

    modified property value

--- object_literals.js
+++ object_literals.js
@@ -2,7 +2,7 @@
 const student1 = {
   name: 'Musa Segun',
   title: 'prefect',
-  year: '2000'
+  year: '2000',
   getSummary: function (){
     return '${this.name} was the ${this.title} in ${this.year}';
   }
