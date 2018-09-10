f1e16ff9515eb840a4b67a8140122dfced5a733d
HamzaJirah <hamzajirah@gmail.com>
2018-09-10 14:12:18 +0100

    added an an object property

--- object_literals.js
+++ object_literals.js
@@ -3,6 +3,9 @@ const student1 = {
   name: 'Musa Segun',
   title: 'prefect',
   year: '2000'
+  getSummary: function (){
+    return '${this.name} was the ${this.title} in ${this.year}';
+  }
 }
 console.log(student1);
 console.log(student1.title);
