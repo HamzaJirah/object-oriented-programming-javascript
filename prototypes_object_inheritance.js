e9c7d979b817181bf5d100ad6ecbf4dfcd602879
HamzaJirah <hamzajirah@gmail.com>
2018-09-11 04:43:04 +0100

    closed statements

--- prototypes_object_inheritance.js
+++ prototypes_object_inheritance.js
@@ -3,12 +3,12 @@ function Student(name, prefect, year){
   this.name = name;
   this.prefect = prefect;
   this.year = year;
-}
+};
 
 //get summary
 Student.prototype.getSummary = function (){
   return `${this.name} has been the ${this.prefect} since ${this.year}`
-}
+};
 
 //books read constructor
 function Friends(name, prefect, year, month){
