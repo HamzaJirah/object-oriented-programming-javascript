6f7b169279e933cf2c2c22e10fd32d72e0357f4b
HamzaJirah <hamzajirah@gmail.com>
2018-09-11 14:10:23 +0100

    added a method

--- object_classes.js
+++ object_classes.js
@@ -4,7 +4,13 @@ class Student {
     this.prefect = prefect;
     this.year = year;
   }
+
+  getSummary (){
+    return `${this.name} is the only reliable ${this.prefect} in our school ${this.year}`;
+  }
 }
 
 //instantiate object
 const student1 = new Student('Hamza Jirah', 'health prefect', '2005'); 
+
+console.log(student1);
