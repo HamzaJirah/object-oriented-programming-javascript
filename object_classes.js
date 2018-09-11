8aa80dc2829552ec7a1bcd66b236bc3cb16d3e25
HamzaJirah <hamzajirah@gmail.com>
2018-09-11 14:15:20 +0100

    created a method

--- object_classes.js
+++ object_classes.js
@@ -8,6 +8,10 @@ class Student {
   getSummary (){
     return `${this.name} is the only reliable ${this.prefect} in our school since ${this.year}`;
   }
+
+  getAge (){
+    
+  }
 }
 
 //instantiate object
