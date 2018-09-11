423011fed45fc9f7831aa8133cc755d5ba255fdb
HamzaJirah <hamzajirah@gmail.com>
2018-09-11 04:33:24 +0100

    modified file

--- object_prototypes.js
+++ object_prototypes.js
@@ -14,17 +14,17 @@ Student.prototype.getSummary = function (){
 Student.prototype.getAge = function(){
   const years = new Date().getFullYear() - this.year;
   return `${this.name} has been the ${this.prefect} for ${years} years`
-}
+};
 
 //Review /Change Year
 Student.prototype.review = function(newYear){
   this.year = newYear;
   this.reviewed = true;
-}
+};
 
 //instantiate object
-const student1 = new Student("Abraham Isa'ac", 'headboy', '2000')
-const student2 = new Student('Maleek Musa', 'house captain', '2003')
+const student1 = new Student("Abraham Isa'ac", 'headboy', '2000');
+const student2 = new Student('Maleek Musa', 'house captain', '2003');
 
 console.log(student2);
 student2.review('2013');
