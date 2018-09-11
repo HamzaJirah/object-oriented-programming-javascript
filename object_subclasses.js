215c5e956144235373cee471d4df8a043e98632f
HamzaJirah <hamzajirah@gmail.com>
2018-09-11 14:57:45 +0100

    removed some methods

--- object_subclasses.js
+++ object_subclasses.js
@@ -8,20 +8,5 @@ class Student {
   getSummary (){
     return `${this.name} is the only reliable ${this.prefect} in our school since ${this.year}`;
   }
-
-  getAge (){
-    const years = new Date().getFullYear() - this.year;
-    return `${this.name} has been the ${this.prefect} for ${years} years`;
-  }
-
-  review (newYear){
-    this.year = newYear;
-    this.reviewed = true;
-  }
-
-  //static method
-  static topStudent(){
-    return `Jirah Ibrahim`;
-  }
 }
 
